638bb61ed80d71046a5103165cde3756da358f37
Ricamslv <64766737+Ricamslv@users.noreply.github.com>
2025-09-02 22:25:49 +0900

    update

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  timeout: 60000, //timeout for each test
-  testDir: 'tests', //directory for the tests
-  testMatch: ['spec.js'], //pattern for the tests
+  timeout: 60000, 
+  testDir: 'tests', 
+  testMatch: ['**/*.spec.js'],
+  reporter: [['html']], 
   use: {
-    headless: false, //for headless mode
-    video: 'on', //retain-on-failure only keep video on failure. On and Off are also available.
-    screenshot: 'on', //only-on-failure. only keep screenshot on failure. On and Off are also available.
+    headless: false, 
+    video: 'on', 
+    screenshot: 'on', 
   },
 }); 
