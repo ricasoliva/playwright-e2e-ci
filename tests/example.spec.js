dd01a32a0cc3346dadd5002d5b146e47596fdf7d
Ricamslv <64766737+Ricamslv@users.noreply.github.com>
2025-09-04 17:58:17 +0900

    fix

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,33 +1,10 @@
 // @ts-check
-import path from 'path';
-import { config } from 'dotenv';
 import { test, expect } from '@playwright/test';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { login } from '../helpers/login.js';
 
-// Get __dirname in ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Only load dotenv in non-CI environments
-if (!process.env.CI) {
-  config({ path: path.join(__dirname, '..', '.env') });
-}
-
-// Make sure required env vars exist
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
-
-if (!username || !password) {
-  throw new Error('Missing USERNAME or PASSWORD environment variables.');
-}
 
 test('login test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.getByPlaceholder('Username').fill(username);
-  await page.getByPlaceholder('Password').fill(password);
-  await page.getByRole('button', { name: 'Login' }).click();
-
+  await login(page);
   // Example assertion
   await expect(page).toHaveURL(/inventory/);
 });
